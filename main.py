30e51c2ad906cfc7a51af02e9b5142ce213f1698
Alexander Melnyk <inboxnumberzero@zoho.com>
2017-02-02 14:53:30 +0200

    Add docstring for the file

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""tagme-file utility.
+
+tagme-file is a program to store arbitrary files with arbitrary tags attached
+to them, and retrieve files by tags or their combinations.
+
+This module should most likely not be imported.
+"""
 
 
 def main():
